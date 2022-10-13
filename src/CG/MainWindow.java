e29442bfeabae131ab3794cb997a0442525c556a
Sergei <adianov@cs.vsu.ru>
2022-10-13 10:21:50 +0300

    Создана DrawPanel для вывода на экран

--- src/CG/MainWindow.java
+++ src/CG/MainWindow.java
@@ -1,4 +1,13 @@
 package CG;
 import javax.swing.*;
+import java.awt.*;
+
 public class MainWindow extends JFrame{
+    private final DrawPanel dp;
+
+    public MainWindow() throws HeadlessException {
+        dp = new DrawPanel();
+        this.add(dp);
+
+    }
 }
